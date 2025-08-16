dbcb368f2a0e1145373768f7454dab36e38b2cda
Astha Shukla <asthashukla160428@gmail.com>
2025-08-16 19:36:16 +0530

    updated mobile toggle menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
             ))}
             <div className="flex space-x-4">
               <a
-                href="https://github.com/codingmastr"
+                href="https://github.com/Asthashukla1"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-white"
@@ -138,12 +138,20 @@ const Navbar = () => {
                 <FaGithub size={24} />
               </a>
               <a
-                href="https://www.linkedin.com/in/tarun-kaushik-553b441a4"
+                href="https://www.linkedin.com/in/astha-shukla-0a036827b/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-white"
               >
                 <FaLinkedin size={24} />
+              </a>
+               <a
+                  href="https://leetcode.com/u/astha_shukla1/"  // replace with your LeetCode ID
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-300 hover:text-[#8245ec]"
+               >
+               <SiLeetcode size={28} />
               </a>
             </div>
           </ul>
